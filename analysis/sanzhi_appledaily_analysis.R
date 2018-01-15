365d8002394946d59c11122102355bfd85f021aa
LU YI <tsb1050391@gmail.com>
2018-01-15 11:42:18 +0800

    Update

--- analysis/sanzhi_appledaily_analysis.R
+++ analysis/sanzhi_appledaily_analysis.R
@@ -67,6 +67,11 @@ dt[, news_text := news_text %>%
      str_replace_all(url_re, "") %>% 
      str_replace_all("\\p{So}|\\p{C}", "")] # Unicode Regular Expressions
 dt[, news_text := news_text %>% str_replace_all(url_re, "")]
+# Remove author
+dt[, news_text := news_text %>%
+     str_replace("([【(（]).*?(更新|新增|報導).*?[】)）]", "")]
+# Remove empty news
+dt <- dt[news_text != ""]
 
 # tokenize ----------------------------------------------------------------
 
@@ -91,7 +96,6 @@ cutter <- function (text, seg_worker, filter_words) {
     return(NA_character_)
   }
   
-  filter_words = filter_words
   pattern <- sprintf("^%s", paste(filter_words, collapse = "|^"))
   tryCatch({
     text_seg <- seg_worker <= text
@@ -103,32 +107,66 @@ cutter <- function (text, seg_worker, filter_words) {
 }
 
 # tokenize
-text_seg <- dt[, news_text] %>% lapply(cutter, mix_seg)
+text_seg <- dt[, news_text] %>% lapply(cutter, mix_seg, filter_words)
 
-# tokenize in parallel
-cl <- makeCluster(detectCores()-1)
-clusterEvalQ(cl, {
-  library(stringr)
-  library(jiebaR)
-  mix_seg <- worker(type = "mix",
-                    user = "dict/dict_utf8.txt",
-                    stop_word = "dict/stop_utf8.txt",
-                    symbol = FALSE,
-                    encoding = "UTF-8")
-})
-clusterExport(cl, list("cutter", "mix_seg"), envir = )
+# # tokenize in parallel
+# cl <- makeCluster(detectCores()-1)
+# clusterEvalQ(cl, {
+#   library(stringr)
+#   library(jiebaR)
+#   mix_seg <- worker(type = "mix",
+#                     user = "dict/dict_utf8.txt",
+#                     stop_word = "dict/stop_utf8.txt",
+#                     symbol = FALSE,
+#                     encoding = "UTF-8")
+# })
+# clusterExport(cl, list("cutter", "mix_seg"), envir = )
+# 
+# text_seg <- dt[1:5, news_text] %>% 
+#   parLapply(cl, ., cutter, mix_seg, filter_words) %>% 
+#   parLapply(cl, ., function(x) x[!is.na(x)])
+# 
+# stopCluster(cl)
 
-text_seg <- dt[1:5, news_text] %>% 
-  parLapply(cl, ., cutter, mix_seg, filter_words) %>% 
-  parLapply(cl, ., function(x) x[!is.na(x)])
-
-stopCluster(cl)
 # adjust to the format for text2vec::itoken
 text_token <- itoken(text_seg)
 
-# 關鍵詞 Top 100 -----------------------------------------------------------
+
+# 新詞探勘 ------------------------------------------------------------------
+
+# unique word matrix
+vocab <- create_vocabulary(text_token, ngram=c(1L, 3L), sep_ngram = "_")
+pruned_vocab <- prune_vocabulary(
+  vocab, term_count_min = 10, doc_proportion_max = 0.8,
+  doc_proportion_min = 0.001, vocab_term_max = 20000)
+
+# dtm
+vectorizer <- vocab_vectorizer(vocab)
+dtm <- create_dtm(text_token, vectorizer)
+# remove 1-word term
+dtm <- dtm[, dtm %>% colnames() %>% nchar >= 2]
+
+# dtm %>% find_freq_terms(30) # not good
+
+# 關鍵詞 Top 100 ------------------------------------------------------------
 
 # 利用 tf-idf 關鍵詞算法，處理高頻詞高估及低頻詞低估的問題，取得整個文檔的關鍵詞
+
+# tf-idf
+tfidf = TfIdf$new() # define tfidf model
+# fit model to train data and transform train data with fitted model
+dtm_train_tfidf = fit_transform(dtm, tfidf)
+# tfidf modified by fit_transform() call!
+
+# Key term
+key_term <- dtm_train_tfidf %>% 
+  find_freq_terms(lowfreq = 0.05) %>% 
+  colSums() %>% 
+  data.frame() %>% 
+  data.table(keep.rownames = TRUE) %>% 
+  setnames(c("keyword", "sum_tf_idf")) %>% 
+  .[order(-sum_tf_idf)]
+key_term %>% head(100) %>% DT::datatable(extensions = "Responsive")
 
 # Wordcloud
 d <- key_term %>% head(200)
